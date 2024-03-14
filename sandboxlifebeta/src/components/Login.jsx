7be09539d28e0423ca50ae5ecfeb23347dfa281b
Mage Narayan <sheshnarayan.iyer@gmail.com>
2024-03-14 22:59:18 +0530

    insital commit
    update

---
+++ sandboxlifebeta/src/components/Login.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+import { loginFields } from "../constants/formFields";
+import FormAction from "./FormAction";
+import FormExtra from "./FormExtra";
+import Input from "./Input";
+import { supabase } from "../utils/supabase";
+
+
+const fields=loginFields;
+let fieldsState = {};
+fields.forEach(field=>fieldsState[field.id]='');
+
+export default function Login(){
+    const [loginState,setLoginState]=useState(fieldsState);
+
+    const handleChange=(e)=>{
+        setLoginState({...loginState,[e.target.id]:e.target.value})
+    }
+    console.log(loginState)
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        authenticateUser();
+    }
+
+    const authenticateUser = async() =>{
+        
+       
+        let { data, error }  = await supabase.auth.signInWithPassword({
+            email: loginState['email-address'],
+            password: loginState.password,
+            
+          })
+          console.log(data,error ,"loginconfirmation")
+         
+    }
+
+    return(
+        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+        <div className="-space-y-px">
+            {
+                fields.map(field=>
+                        <Input
+                            key={field.id}
+                            handleChange={handleChange}
+                            value={loginState[field.id]}
+                            labelText={field.labelText}
+                            labelFor={field.labelFor}
+                            id={field.id}
+                            name={field.name}
+                            type={field.type}
+                            isRequired={field.isRequired}
+                            placeholder={field.placeholder}
+                    />
+                
+                )
+            }
+        </div>
+
+        <FormExtra/>
+        <FormAction handleSubmit={handleSubmit} text="Login"/>
+
+      </form>
+    )
+}
---
+++ sandboxlifebeta/src/utils/supabase.jsx
@@ -0,0 +1,4 @@
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://teyudjxlutkavyyigwwz.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRleXVkanhsdXRrYXZ5eWlnd3d6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDYzMDQ2NjgsImV4cCI6MjAyMTg4MDY2OH0.MvaDhHKE55sSIEiasenRbR9U1LKnt7ae6dZUa89LUJg'
+export const supabase = createClient(supabaseUrl, supabaseKey)
