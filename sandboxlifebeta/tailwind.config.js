d650eadccb4ccacf56f3fabed1d26cb768e4d834
Mage Narayan <sheshnarayan.iyer@gmail.com>
2024-03-15 17:56:38 +0530

    Update tailwind.config.js

--- sandboxlifebeta/tailwind.config.js
+++ sandboxlifebeta/tailwind.config.js
@@ -6,18 +6,7 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        'papyrus': {
-          DEFAULT: '#f5f5dc', // A papyrus-like beige
-          'light': '#fafaf0', // A lighter shade of papyrus
-          'dark': '#e5e5c7'  // A darker shade of papyrus
-        },
-        'scroll': {
-          DEFAULT: '#d2b48c', // A tan color similar to aged paper
-          'light': '#e5cda0', // A lighter tan
-          'dark': '#b09a6b'  // A darker tan
-        },
-        // ... other colors from your palette ...
+       
 
     },
   },
