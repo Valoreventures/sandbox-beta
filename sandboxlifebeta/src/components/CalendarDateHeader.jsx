ca761a90c41482e335970d909673c59b27672631
Mage Narayan <sheshnarayan.iyer@gmail.com>
2024-03-15 23:28:05 +0530

    Update CalendarDateHeader.jsx

--- sandboxlifebeta/src/components/CalendarDateHeader.jsx
+++ sandboxlifebeta/src/components/CalendarDateHeader.jsx
@@ -3,11 +3,19 @@ import React from 'react';
 const CalendarComponent = () => {
   const daysOfWeek = ['Thu', 'Fri', 'Sat', 'Sun', 'Mon', 'Tue', 'Wed'];
   const icons = [
-    null, // Thursday (no icon)
-    null, // Friday (no icon)
+    <img
+      src="http://www.sandboxlife.com/images/icons/snake.jpg"
+      alt="Snake"
+      className="h-6 w-6"
+    />, // Thursday (no icon)
+    <img
+      src="http://www.sandboxlife.com/images/icons/lotus.jpg"
+      alt="Lotus"
+      className="h-6 w-6"
+    />, // Friday (no icon)
     <img
       src="http://www.sandboxlife.com/images/icons/bird.jpg"
-      alt="Butterfly"
+      alt="bird"
       className="h-6 w-6"
     />, // Saturday
     [
@@ -16,17 +24,29 @@ const CalendarComponent = () => {
       </div>,
       <img
         src="http://www.sandboxlife.com/images/icons/lotus.jpg"
-        alt="Lighthouse"
+        alt="Lotus"
         className="h-6 w-6"
       />,
     ], // Sunday
-    null, // Monday (no icon)
-    null, // Tuesday (no icon)
-    null, // Wednesday (no icon)
+    <img
+      src="http://www.sandboxlife.com/images/icons/key.jpg"
+      alt="Access"
+      className="h-6 w-6"
+    />, // Monday (no icon)
+    <img
+      src="http://www.sandboxlife.com/images/icons/trophy.jpg"
+      alt="Trophy"
+      className="h-6 w-6"
+    />, // Tuesday (no icon)
+    <img
+      src="http://www.sandboxlife.com/images/icons/bird.jpg"
+      alt="Butterfly"
+      className="h-6 w-6"
+    />, // Wednesday (no icon)
   ];
 
   return (
-    <div className=" fixed w-full flex justify-between items-center right-0 top-8 bg-lightpapyrus">
+    <div className=" fixed w-full flex justify-between items-center right-0 top-8 bg-lightpapyrus px-10 py-10">
       {daysOfWeek.map((day, index) => (
         <div key={index} className="flex flex-col items-center">
           <span className="text-gray-600 font-medium mb-2">{day}</span>
